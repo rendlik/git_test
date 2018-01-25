b09e894b6e9b16ae5c8c573831e973e5c9d9c6e3
rendlik <rsonroad@gmail.com>
2018-01-25 07:29:34 +0100

    fundahmer script uses jQuery

--- fundahmer/js/script.js
+++ fundahmer/js/script.js
@@ -1,17 +1,16 @@
-document.getElementById("menu-button").addEventListener("click", function(){
-	var logo = document.getElementById('main-logo');
-
-	if(logo.style.visibility == "visible" || logo.style.visibility == ""){
-		logo.style.visibility = "hidden";
+$("#menu-button").click(function(){
+	const logo = $('#main-logo');
+	if(logo.css('visibility') == "" || logo.css('visibility') == "visible"){
+		logo.css('visibility','hidden');
 	}
 	else{
-		logo.style.visibility = "visible";
+		logo.css('visibility','visible');
 	}
 });
 
 $(function(){
 	$("#menu-button").blur(function(event){
-		var screenWidth = window.innerWidth;
+		const screenWidth = window.innerWidth;
 		if (screenWidth < 768) {
 			$("#bs-example-navbar-collapse-1").collapse('hide');
 			$("#main-logo").css("visibility","visible");
