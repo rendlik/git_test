445f92872921a9f2b18c66a5c9289ad910bbda5d
rendlik <rsonroad@gmail.com>
2018-01-26 07:19:58 +0100

    contact form added

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,9 +1,15 @@
 const closeButtons = document.querySelectorAll('.close-button')
 const projectsList = document.querySelector('.projects-list')
 const projects = document.querySelector('.projects')
+const contactForm = document.querySelector('.contact-form')
+const contact = document.querySelector('.contact')
 
 projects.addEventListener('click',function(){
     projectsList.classList.add('open')
+})
+
+contact.addEventListener('click',function(){
+    contactForm.classList.add('open')
 })
 
 closeButtons.forEach(button => button.addEventListener('click',function(){
