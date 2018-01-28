4be4f349db69e80c27d020d2f651dc01d4a14f39
rendlik <rsonroad@gmail.com>
2018-01-28 21:44:28 +0100

    mail changed to mailto

--- portfolio/main.js
+++ portfolio/main.js
@@ -7,7 +7,7 @@ const projectOverlay = document.querySelector('.project-overlay')
 const projectText = document.querySelectorAll('.project p')
 const contactFormSubmit = document.querySelector('.contact-form form')
 const name = document.querySelector('[name="name"]')
-console.log(name)
+
 
 
 projects.addEventListener('click',function(){
@@ -39,10 +39,14 @@ function sendEmail(e){
     const name = document.querySelector('[name="name"]')
     const email = document.querySelector('[name="email"]')
     const message = document.querySelector('[name="message"]')
+    const confirmation = document.querySelector('.confirmation')
+    const error = document.querySelector('.error')
+    
 
     if(!name.value || !message.value){
-
+        error.style.display = 'block'
     }else{
+        console.log('ahoj')
         $.ajax({
             url: "https://formspree.io/rmnstix@gmail.com", 
             method: "POST",
@@ -51,6 +55,10 @@ function sendEmail(e){
         });
         e.preventDefault()
         $(this).get(0).reset()
+        confirmation.style.display = 'block'
+        setTimeout(function(){
+            confirmation.style.display ='none'
+        },2500)
     }
 }
 
