27a677de7200b215aacdf1b492853a68cf992af0
rendlik <rsonroad@gmail.com>
2018-01-28 08:59:33 +0100

    form sending emails

--- portfolio/main.js
+++ portfolio/main.js
@@ -5,6 +5,9 @@ const contactForm = document.querySelector('.contact-form')
 const contact = document.querySelector('.contact')
 const projectOverlay = document.querySelector('.project-overlay')
 const projectText = document.querySelectorAll('.project p')
+const contactFormSubmit = document.querySelector('.contact-form form')
+const name = document.querySelector('[name="name"]')
+console.log(name)
 
 
 projects.addEventListener('click',function(){
@@ -32,4 +35,23 @@ function darkenImage(){
 projectText.forEach(text => text.addEventListener('mouseenter',lightenImage))
 projectText.forEach(text => text.addEventListener('mouseleave',darkenImage))
 
+function sendEmail(e){
+    const name = document.querySelector('[name="name"]')
+    const email = document.querySelector('[name="email"]')
+    const message = document.querySelector('[name="message"]')
 
+    if(!name.value || !message.value){
+
+    }else{
+        $.ajax({
+            url: "https://formspree.io/rmnstix@gmail.com", 
+            method: "POST",
+            data: $(this).serialize(),
+            dataType: "json"
+        });
+        e.preventDefault()
+        $(this).get(0).reset()
+    }
+}
+
+contactFormSubmit.addEventListener('submit',sendEmail)
