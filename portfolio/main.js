b30b3293cfed561e6bb881718afa5f01701a8788
rendlik <rsonroad@gmail.com>
2018-01-29 05:32:50 +0100

    responsive design implemented

--- portfolio/main.js
+++ portfolio/main.js
@@ -7,15 +7,18 @@ const projectOverlay = document.querySelector('.project-overlay')
 const projectText = document.querySelectorAll('.project p')
 const contactFormSubmit = document.querySelector('.contact-form form')
 const name = document.querySelector('[name="name"]')
-
+console.log(projects)
 
 
 projects.addEventListener('click',function(){
+    if(screen.width < 681) return
+    console.log(screen.width)
     projectsList.classList.add('open')
     projectOverlay.classList.add('open')
 })
 
 contact.addEventListener('click',function(){
+    if(screen.width < 681) return
     contactForm.classList.add('open')
 })
 
@@ -46,7 +49,6 @@ function sendEmail(e){
     if(!name.value || !message.value){
         error.style.display = 'block'
     }else{
-        console.log('ahoj')
         $.ajax({
             url: "https://formspree.io/rmnstix@gmail.com", 
             method: "POST",
@@ -61,5 +63,41 @@ function sendEmail(e){
         },2500)
     }
 }
+
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
 
 contactFormSubmit.addEventListener('submit',sendEmail)
