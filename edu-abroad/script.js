41eca69187924c87140749af59d5eeff3f65335b
rendlik <rsonroad@gmail.com>
2018-02-20 22:34:06 -0800

    overlay div added, needs to be styled

--- edu-abroad/script.js
+++ edu-abroad/script.js
@@ -5,9 +5,11 @@ const closeButtons = document.querySelectorAll('#close-button')
 const countriesDetail = document.querySelectorAll('.country-detail') 
 const contactLink = document.querySelector('#contact')
 const contactForm = document.querySelector('.contact')
+const overlay = document.querySelector(".overlay")
 
 menuButton.addEventListener('click',function(){
 	menuButton.classList.toggle('active')
+	overlay.classList.toggle('open')
 })
 
 menuButton.addEventListener('click',function(){
@@ -17,10 +19,12 @@ menuButton.addEventListener('click',function(){
 countries.forEach(country => country.addEventListener('click',function(){
 	console.log(this.nextSibling.nextSibling)
 	this.nextSibling.nextSibling.classList.add('open')
+	overlay.classList.add('open')
 }))
 
 closeButtons.forEach(button => button.addEventListener('click',function(){
 	this.parentNode.classList.remove('open')
+	overlay.classList.remove('open')
 }))
 
 document.addEventListener('keydown',function(e){
