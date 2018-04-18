bcccae9345691cb9db4c7890f5ca41b23ad48454
rendlik <rsonroad@gmail.com>
2018-04-18 13:27:02 -0700

    small details changed

--- edu-abroad/script.js
+++ edu-abroad/script.js
@@ -10,8 +10,9 @@ const scholarshipsForm = document.querySelector('.scholarships')
 const overlay = document.querySelector(".overlay")
 const scholarshipsLinks = document.querySelectorAll(".nav")
 const scholarshipsDesc = document.querySelectorAll(".scholar")
+const contactFormSubmit = document.querySelector('.contact-detail form')
 
-console.log(scholarshipsDesc)
+console.log(contactFormSubmit)
 
 scholarshipsLinks.forEach(link => link.addEventListener("click",function(){
 	scholarshipsLinks.forEach(link=>link.classList.remove("open"))
@@ -77,3 +78,63 @@ scholarshipsLink.addEventListener('click',function(){
 	menu.classList.remove('open')
 	menuButton.classList.remove('active')
 })
+
+
+function sendEmail(e){
+    const name = document.querySelector('[name="name"]')
+    const email = document.querySelector('[name="email"]')
+    const message = document.querySelector('[name="message"]')
+    const confirmation = document.querySelector('.confirmation')
+    const error = document.querySelector('.error')
+    
+
+    if(!name.value || !message.value){
+        error.style.display = 'block'
+    }else{
+        $.ajax({
+            url: "https://formspree.io/rmnstix@gmail.com", 
+            method: "POST",
+            data: $(this).serialize(),
+            dataType: "json"
+        });
+        e.preventDefault()
+        $(this).get(0).reset()
+        confirmation.style.display = 'block'
+        setTimeout(function(){
+            confirmation.style.display ='none'
+        },2500)
+    }
+}
+
+$('a[href*="#"]')
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    let target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        let $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { 
+          return false;
+        } else {
+          $target.attr('tabindex','-1');
+          $target.focus(); 
+        };
+      });
+    }
+  }
+});
+
+contactFormSubmit.addEventListener('submit',(e)=> {
+	e.preventDefault()
+	console.log(1)})
