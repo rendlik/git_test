533bcb8b2a38504665086a9817b32120b513eee9
rendlik <rsonroad@gmail.com>
2018-04-18 21:19:13 -0700

    backend done

--- edu-abroad/script.js
+++ edu-abroad/script.js
@@ -5,14 +5,14 @@ const closeButtons = document.querySelectorAll('#close-button')
 const countriesDetail = document.querySelectorAll('.country-detail') 
 const contactLink = document.querySelector('#contact')
 const contactForm = document.querySelector('.contact')
-const scholarshipsLink = document.querySelector('#scholarships')
+const scholarshipsLink = document.querySelectorAll('#scholarships')
 const scholarshipsForm = document.querySelector('.scholarships')
 const overlay = document.querySelector(".overlay")
 const scholarshipsLinks = document.querySelectorAll(".nav")
 const scholarshipsDesc = document.querySelectorAll(".scholar")
 const contactFormSubmit = document.querySelector('.contact-detail form')
 
-console.log(contactFormSubmit)
+
 
 scholarshipsLinks.forEach(link => link.addEventListener("click",function(){
 	scholarshipsLinks.forEach(link=>link.classList.remove("open"))
@@ -38,7 +38,7 @@ menuButton.addEventListener('click',function(){
 })
 
 countries.forEach(country => country.addEventListener('click',function(){
-	console.log(this.nextSibling.nextSibling)
+
 	this.nextSibling.nextSibling.classList.add('open')
 	overlay.classList.add('open')
 }))
@@ -73,68 +73,97 @@ contactLink.addEventListener('click',function(){
 	menuButton.classList.remove('active')
 })
 
-scholarshipsLink.addEventListener('click',function(){
+scholarshipsLink.forEach(link=> link.addEventListener('click',function(){
+	countriesDetail.forEach(country=>country.classList.remove('open'))
 	scholarshipsForm.classList.add('open')
 	menu.classList.remove('open')
 	menuButton.classList.remove('active')
-})
+}))
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA9uVssie1b9y8ctPaen9xunKHsCWGv1OQ",
+    authDomain: "edugo-3e5cc.firebaseapp.com",
+    databaseURL: "https://edugo-3e5cc.firebaseio.com",
+    projectId: "edugo-3e5cc",
+    storageBucket: "edugo-3e5cc.appspot.com",
+    messagingSenderId: "300557804365"
+  };
+  firebase.initializeApp(config);
+
+var messageRef = firebase.database().ref('messages')
 
 
 function sendEmail(e){
-    const name = document.querySelector('[name="name"]')
-    const email = document.querySelector('[name="email"]')
-    const message = document.querySelector('[name="message"]')
+	e.preventDefault()
+    const name = document.querySelector('[name="name"]').value
+    const email = document.querySelector('[name="email"]').value
+    const message = document.querySelector('[name="message"]').value
     const confirmation = document.querySelector('.confirmation')
     const error = document.querySelector('.error')
     
 
-    if(!name.value || !message.value){
+    if(!email || !message){
         error.style.display = 'block'
     }else{
-        $.ajax({
-            url: "https://formspree.io/rmnstix@gmail.com", 
-            method: "POST",
-            data: $(this).serialize(),
-            dataType: "json"
-        });
-        e.preventDefault()
-        $(this).get(0).reset()
-        confirmation.style.display = 'block'
-        setTimeout(function(){
-            confirmation.style.display ='none'
-        },2500)
-    }
+		saveMessage(name,email,message)
+		error.style.display = 'none'
+		$(this).get(0).reset()
+		confirmation.style.display = 'block'
+		         setTimeout(function(){
+		             confirmation.style.display ='none'
+		         },2500)
+//         $.ajax({
+//             url: "https://formspree.io/rmnstix@gmail.com", 
+//             method: "POST",
+//             data: $(this).serialize(),
+//             dataType: "json"
+//         });
+
+//         $(this).get(0).reset()
+//         confirmation.style.display = 'block'
+//         setTimeout(function(){
+//             confirmation.style.display ='none'
+//         },2500)
+//     }
+// }
+
+// $('a[href*="#"]')
+// .not('[href="#"]')
+// .not('[href="#0"]')
+// .click(function(event) {
+//   if (
+//     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+//     && 
+//     location.hostname == this.hostname
+//   ) {
+//     let target = $(this.hash);
+//     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//     if (target.length) {
+//       event.preventDefault();
+//       $('html, body').animate({
+//         scrollTop: target.offset().top
+//       }, 1000, function() {
+//         let $target = $(target);
+//         $target.focus();
+//         if ($target.is(":focus")) { 
+//           return false;
+//         } else {
+//           $target.attr('tabindex','-1');
+//           $target.focus(); 
+//         };
+//       });
+//     }
+ }
+};
+
+function saveMessage(name,email,message){
+	let newMessageRef = messageRef.push()
+	newMessageRef.set({
+		name: name,
+		email:email,
+		message: message
+	})
 }
 
-$('a[href*="#"]')
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function(event) {
-  if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
-    location.hostname == this.hostname
-  ) {
-    let target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        let $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { 
-          return false;
-        } else {
-          $target.attr('tabindex','-1');
-          $target.focus(); 
-        };
-      });
-    }
-  }
-});
-
-contactFormSubmit.addEventListener('submit',(e)=> {
-	e.preventDefault()
-	console.log(1)})
+contactFormSubmit.addEventListener('submit',sendEmail)
