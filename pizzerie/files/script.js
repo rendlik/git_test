7b814552fc95f199610a2267c44a0e22222913ce
rendlik <rsonroad@gmail.com>
2018-04-28 22:01:26 -0700

    smooth scroll

--- pizzerie/files/script.js
+++ pizzerie/files/script.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
     const scrollLinks = $('.scroll')
     
+    //smooth scrolling to the section
     scrollLinks.click(function(e){
         e.preventDefault();
         $('body,html').animate({
             scrollTop: $(this.hash).offset().top - 50 +'px'
         },1000)
+    })
+
+    //active link from menu higlighterd
+    $(window).scroll(function(){
+        const scrollbarLocation = $(this).scrollTop()
+
+        scrollLinks.each(function(){        
+            const sectionOffset = $(this.hash).offset().top-0.5*$(window).height();
+            console.log(scrollbarLocation)
+            if (sectionOffset <= scrollbarLocation){
+                $(this).addClass('active')
+                $(this).siblings().removeClass('active')
+            }else if(scrollbarLocation < 0.5*$(window).height()){
+                $(this).removeClass('active')
+            }
+        })
     })
 })
