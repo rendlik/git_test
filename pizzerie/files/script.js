47d222da6b56365d1f3c2c0f857ab3e86043f2f7
rendlik <rsonroad@gmail.com>
2018-04-29 01:20:35 -0700

    sample done

--- pizzerie/files/script.js
+++ pizzerie/files/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     const scrollLinks = $('.scroll')
-    
+
     //smooth scrolling to the section
     scrollLinks.click(function(e){
         e.preventDefault();
         $('body,html').animate({
             scrollTop: $(this.hash).offset().top - 50 +'px'
-        },1000)
+        },800)
     })
 
     //active link from menu higlighterd
@@ -15,10 +15,7 @@ $(document).ready(function(){
 
         scrollLinks.each(function(){        
             const sectionOffset = $(this.hash).offset().top-0.5*$(window).height();
-            console.log(scrollbarLocation)
             if (sectionOffset <= scrollbarLocation){
-                console.log(scrollbarLocation)
-                console.log($(this))
                 $(this).addClass('active')
                 $(this).siblings().removeClass('active')
             }else if(scrollbarLocation < 0.5*$(window).height()){
