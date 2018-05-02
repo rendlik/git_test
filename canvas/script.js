1dd9acfdf979707fc43e35cc7904a34e05279d67
rendlik <rsonroad@gmail.com>
2018-05-02 10:13:50 -0700

    canvas

--- canvas/script.js
+++ canvas/script.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector("canvas")
 
 canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.height = window.innerHeight 
 
 const c = canvas.getContext("2d")
 
@@ -10,28 +10,34 @@ const mouse = {
     y: undefined
 }
 
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight  
+    init()
+})
+
 window.addEventListener('mousemove',function(e){
     mouse.x = e.x
     mouse.y = e.y 
 })
 
-function Circle(x,y,dx,dy,radius,r,b,g){
+const colorArr = ['#F4640D','#F6A30B','#F7EC11','#1CEDF0','#127B9A']
+
+function Circle(x,y,dx,dy,radius){
     this.x = x
     this.dx = dx
     this.y = y
     this.dy = dy
     this.radius = radius
-    this.r = r
-    this.b = b
-    this.g = g
+    this.minRadius = radius
+    this.color = colorArr[Math.floor(Math.random()*colorArr.length)]
 
     this.draw = function(){
         c.beginPath()
         c.arc(this.x,this.y,this.radius,0,Math.PI*2,false)
-        c.fillStyle = 'rgb(' + this.r + ',' +
-        this.b + ','+ this.g +')';
+        c.fillStyle = this.color
         c.fill()
-        c.stroke()
+        
        
     }
 
@@ -46,12 +52,12 @@ function Circle(x,y,dx,dy,radius,r,b,g){
         this.x += this.dx
         this.y += this.dy
 
-        if(Math.abs(mouse.x - this.x) < 50 && Math.abs(mouse.y - this.y) < 50){
+        if((mouse.x - this.x) < 50 && mouse.x - this.x > -50 && (mouse.y - this.y) < 50 && mouse.y - this.y > -50){
             if(this.radius< 40){
                 this.radius += 1
             }
    
-        }else if (this.radius > 2){
+        }else if (this.radius > this.minRadius){
             this.radius -= 1
         }
 
@@ -61,21 +67,18 @@ function Circle(x,y,dx,dy,radius,r,b,g){
 
 let circleArr = []
 
+function init(){
+    circleArr = []
+    for(let i = 0; i < 400 ;i++){
+        let radius = Math.random()*4+1
+        let x = Math.random()*(innerWidth - 2*radius) +radius
+        let y = Math.random()*(innerHeight - 2*radius) +radius
+        let dx = (Math.random()-0.5)*4
+        let dy = (Math.random()-0.5)*4
 
-for(let i = 0; i < 700 ;i++){
-    let radius = 27
-    let x = Math.random()*(innerWidth - 2*radius) +radius
-    let y = Math.random()*(innerHeight - 2*radius) +radius
-    let dx = (Math.random()-0.5)*12
-    let dy = (Math.random()-0.5)*12
-    let r =  Math.floor((Math.random())*255) +1
-    let b = Math.floor((Math.random())*255) +1
-    let g = Math.floor((Math.random())*255) +1
-    
-
-    circleArr.push(new Circle(x,y,dx,dy,radius,r,b,g))
+        circleArr.push(new Circle(x,y,dx,dy,radius))
+    }
 }
-
 
 
 function animate(){
@@ -95,5 +98,5 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
   }
     
-
+init()
 animate()
