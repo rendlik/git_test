7575709ab04e6de833737dd64a2816889c161b13
rendlik <rsonroad@gmail.com>
2018-05-24 10:47:30 -0700

    natare menu

--- nata-atelier/script.js
+++ nata-atelier/script.js
@@ -2,10 +2,19 @@ const hamburger = document.querySelector('#hamburger')
 const menu = document.querySelector('#menu')
 const title = document.querySelector('.title')
 
-let scrollTop = 0
+let open = 0
 
 hamburger.addEventListener('click',(e)=>{
-    console.log(e.scrollTop)
+    if(window.pageYOffset > 0 && !open){
+        // title.style.transition = 'all 900ms cubic-bezier(0, 0.65, 0.58, 1)'
+        title.style.top = window.pageYOffset + 'px'
+        open = 1
+    }else{
+        title.style.top = 0;
+        open = 0;
+    }
+
+    console.log(window.pageYOffset)
     hamburger.classList.toggle('active')
     menu.classList.toggle('active')
     document.body.classList.toggle('stop-scrolling')
