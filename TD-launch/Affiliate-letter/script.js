09d9ed063d388e70a321db2f76c3b3602895dd6c
rendlik <rsonroad@gmail.com>
2018-07-11 20:33:48 -0700

    m

--- TD-launch/Affiliate-letter/script.js
+++ TD-launch/Affiliate-letter/script.js
@@ -1,12 +1,26 @@
-const nav = document.querySelector('.video');
-    let topOfNav = nav.offsetTop;
+ const nav = document.querySelector('.video');
+ const navWidth = nav.offsetWidth 
+ const main = document.querySelector('.main');
+ const mainWidth = main.offsetWidth - 40
+ 
+ console.log(navWidth)
+    const navOffsetTop = nav.offsetTop
     function fixNav() {
-      if (window.scrollY >= topOfNav) {
-        document.body.style.paddingTop = nav.offsetHeight + 'px';
-        document.body.classList.add('fixed');
-      } else {
-        document.body.classList.remove('fixed');
-        document.body.style.paddingTop = 0;
+      if(navOffsetTop <= window.scrollY){
+        // if(done){
+         
+        //   done = false
+        // } 
+        console.log(1)
+        // document.querySelector('.main').style.width = navWidth + 'px'
+        nav.style.width = mainWidth + 'px'
+        document.querySelector('.main').style.paddingTop = nav.offsetHeight +'px';
+        nav.classList.add("fixed");
+        
+      }else{
+        document.querySelector('.main').style.paddingTop = 0
+        nav.classList.remove("fixed");
+        // done = true
       }
     }
     window.addEventListener('scroll', fixNav);
