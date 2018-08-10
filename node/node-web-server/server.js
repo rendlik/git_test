de6a500988f474aadbed4c229a0dd0cb52960f54
rendlik <rsonroad@gmail.com>
2018-08-09 17:45:30 -0700

    server

---
+++ node/node-web-server/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const hbs = require('hbs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs')
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase()
+})
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my page'
+    })
+})
+
+app.get('/about', (req,res) =>{
+    res.render('about.hbs',{
+        pageTitle: 'Abou Page'
+    })
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage: 'Unable to connect'
+    })
+})
+
+app.listen(3000)
