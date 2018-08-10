c5bb98d2cc3f554bf58dfa4e49583af1f8a4b693
insspiratech <insspiratech@insspiratechs-MacBook-Pro.local>
2018-08-10 13:47:22 -0700

    maintenance page created

--- node/node-web-server/server.js
+++ node/node-web-server/server.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine','hbs')
+
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+
+    fs.appendFile('server.log',log + '\n',(err)=>{
+        if(err){
+            console.log("Error")
+        }
+    })
+    next();
+})
+
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs')
+})
+
 app.use(express.static(__dirname + '/public'))
 
 hbs.registerHelper('getCurrentYear', ()=>{
@@ -34,4 +53,6 @@ app.get('/bad',(req,res)=>{
     })
 })
 
-app.listen(3000)
+app.listen(3000,()=>{
+    console.log("Server running on port 3000")
+})
