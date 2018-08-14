4a4da27b1dee66df536b9aa6fc2dbcbf37e91931
insspiratech <insspiratech@insspiratechs-MacBook-Pro.local>
2018-08-14 11:16:32 -0700

    testing done

--- node/node-tests/utils/utils.test.js
+++ node/node-tests/utils/utils.test.js
@@ -2,29 +2,32 @@ const expect = require('expect')
 
 const utils = require('./utils')
 
-it('should add two numbers',() =>{
-    var res = utils.add(33,11)
-    expect(res).toBe(44).toBeA('number')
-})
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4,3, (sum)=> {
-        expect(sum).toBe(7)
-        done()
+describe('Utils', () =>{
+    it('should add two numbers',() =>{
+        var res = utils.add(33,11)
+        expect(res).toBe(44).toBeA('number')
+    })
+    
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(4,3, (sum)=> {
+            expect(sum).toBe(7)
+            done()
+        })
+    })
+    
+    it('should square a number',()=>{
+        var res = utils.square(2)
+        expect(res).toBe(4).toBeA("number")
+    })
+    
+    it('should async square a number',(done)=>{
+        utils.asyncSquare(3,(square) =>{
+            expect(square).toBe(9).toBeA('number')
+            done()
+        })
     })
 })
 
-it('should square a number',()=>{
-    var res = utils.square(2)
-    expect(res).toBe(4).toBeA("number")
-})
-
-it('should async square a number',(done)=>{
-    utils.asyncSquare(3,(square) =>{
-        expect(square).toBe(9).toBeA('number')
-        done()
-    })
-})
 
 it('should expect sime values', () => {
     // expect(12).toNotBe(11)
