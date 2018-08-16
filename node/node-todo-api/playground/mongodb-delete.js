82c9c7a452d30e7fdfbe9828936a8132b8298634
insspiratech <insspiratech@insspiratechs-MacBook-Pro.local>
2018-08-16 12:30:07 -0700

    add delete script

--- node/node-todo-api/playground/mongodb-delete.js
+++ node/node-todo-api/playground/mongodb-delete.js
@@ -7,7 +7,16 @@ MongoClient.connect('mongodb://localhost:27017',(err,client) => {
 
     let db = client.db('TodoApp')
 
-    db.collection('Todos').deleteMany({name: 'eat luch'})
+    // db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+    //     console.log(result)
+    // })
 
+    // db.collection('Todos').deleteOne({text: 'walk the dog'}).then(result => console.log(result))
+
+    // db.collection('Users').findOneAndDelete({name: 'Jan'}).then(result => console.log(result))
+
+    // db.collection('Users').deleteMany({name: 'Roman'}).then(result => console.log(result))
+
+    db.collection('Users').findOneAndDelete({name: 'Jan'})
     client.close()
 })
