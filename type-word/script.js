b131ea3709b6e5910bc8287a6aa08211eee53f3e
rendlik <rsonroad@gmail.com>
2018-08-19 09:42:34 -0700

    type game

---
+++ type-word/script.js
@@ -0,0 +1,8 @@
+let time = 5;
+let score = 0;
+let isPlaying;
+
+const wordInput = document.querySelector('#input');
+const score = document.querySelector('#score');
+const timeLeft = document.querySelector('#time-left');
+const word = document.querySelector('#word')
