1a2543c372664b00965ca0ac135cac24fa9d1f1c
rendlik <rsonroad@gmail.com>
2018-08-19 09:49:19 -0700

    type game init

--- type-word/script.js
+++ type-word/script.js
@@ -1,8 +1,22 @@
+window.addEventListener('load',init)
+
 let time = 5;
 let score = 0;
 let isPlaying;
 
 const wordInput = document.querySelector('#input');
-const score = document.querySelector('#score');
+const scoreCounter = document.querySelector('#score');
 const timeLeft = document.querySelector('#time-left');
-const word = document.querySelector('#word')
+const word = document.querySelector('#word');
+
+const words = [
+    'hello',
+    'dog',
+    'table',
+    'city',
+    'apple'
+]
+
+function init(){
+    console.log(words)
+}
