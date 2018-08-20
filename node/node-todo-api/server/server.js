4ae1d01cb1af4eb76b2d4cf1cf5fa5709b72ee75
rendlik <rsonroad@gmail.com>
2018-08-19 20:35:40 -0700

    add GET /todo/:id

--- node/node-todo-api/server/server.js
+++ node/node-todo-api/server/server.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const bodyParser = require('body-parser')
 
+const {ObjectID} = require('mongodb')
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
@@ -19,6 +20,34 @@ app.post('/todos',(req,res)=>{
     },(e)=>{
         res.status(400).send(e)
     })
+})
+
+app.post('/user',(req,res) => {
+    let user = new User({
+        email : req.body.email
+    })
+
+    user.save().then((doc) => {
+        res.send(doc)
+    },(e) =>{
+        res.status(400).send(e)
+    })
+})
+
+app.get('/todos/:id',(req,res)=>{
+   const id = req.params.id
+
+   if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+   }
+
+   Todo.findById(id).then(todo=>{
+       if(!todo){
+           res.status(404).send()
+       }else{
+           res.send(todo)
+       }
+   }).catch(e => res.status(404).send())
 })
 
 app.get('/todos',(req,res)=>{
