4811c885779cb164a29a2b3705d7301acce57290
rendlik <rsonroad@gmail.com>
2018-08-19 19:18:43 -0700

    GET route added

--- node/node-todo-api/server/tests/server.test.js
+++ node/node-todo-api/server/tests/server.test.js
@@ -2,7 +2,11 @@ const expect = require('expect')
 const request = require('supertest')
 
 const {app} = require('../server')
-const {Todo} = require('./../models/tood')
+const {Todo} = require('./../models/todo')
+
+beforeEach((done) => {
+    Todo.remove({}).then(() => done())
+})
 
 describe('POST /todos', ()=>{
     it('should create a new todo',(done) => {
@@ -17,7 +21,7 @@ describe('POST /todos', ()=>{
             })
             .end((err,res)=>{
                 if(err){
-                    retrurn done(err)
+                    return done(err)
                 }
 
                 Todo.find().then((todos)=>{
@@ -25,6 +29,23 @@ describe('POST /todos', ()=>{
                     expect(todos[0].text).toBe(text)
                     done()
                 }).catch((e)=>done(e))
+            })
+    })
+
+    it('should not create a todo with invalid data',(done)=>{
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err,res) => {
+                if(err){
+                    return done(err)
+                }
+
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(0)
+                done()
+            }).catch(e=>done(e))
             })
     })
 })
--- type-word/script.js
+++ type-word/script.js
@@ -18,5 +18,56 @@ const words = [
 ]
 
 function init(){
-    console.log(words)
+    //load random word 
+    showWord(words)
+    //start matching on word input
+    wordInput.addEventListener('input',startMatch)
+    //call countdown every second
+    setInterval(countdown,1000)
+    //check game status
+    setInterval(checkStatus,50)
+}
+
+function showWord(words){
+    const randomIndex = Math.floor(Math.random()*words.length)
+
+    word.innerHTML = words[randomIndex]
+}
+
+function countdown(){
+    if(time>0){
+        time--
+    }else if (time === 0){
+        isPlaying = false;
+    }
+    timeLeft.innerHTML = time;
+}
+
+function checkStatus(){
+    if(!isPlaying & time === 0){
+       // message.innerHTML = 'Game Over!!'
+       score = -1;
+    }
+}
+
+function startMatch(){
+    if(matchWord()){
+       console.log('MATCH')
+       isPlaying = true
+       time = 6
+       showWord(words)
+       wordInput.value = ''
+       score++
+    }
+    scoreCounter.innerHTML = score === -1 ? 0 : score
+}
+
+function matchWord(){
+    if(wordInput.value === word.innerHTML){
+        // message.innerHTML = 'Correct!!!'
+        return true
+    } else{
+        // message.innerHTML = ''
+        return false
+    }
 }
